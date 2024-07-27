b3ce19992984aa8eef328ba2ae61eb3b7afb6855
KGOTSO30 <96039328+KGOTSO30@users.noreply.github.com>
2024-07-27 06:44:44 +0200

    navBar init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent
   ],
   imports: [
+   
     BrowserModule,
     AppRoutingModule
   ],
